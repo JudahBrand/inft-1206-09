82c1b87e1f81161b307dcde07c25e5620d53e48f
JudahBrand <156016875+JudahBrand@users.noreply.github.com>
2024-03-22 15:10:56 -0400

    Placeholder changes

--- placeholder/main.js
+++ placeholder/main.js
@@ -1,6 +1,22 @@
+const myName = 'Judah Brand';
+const myAge = 19;
+let pTagQuery = document.querySelector('p');
+let pTagId = document.getElementById('display');
 
 
+console.log(`Hello From ${ myName }`);
 
+function add(x, y) {
+    return x + y
+}
 
+// TODO: create a big arrow function
+addTwo = (x, y) => {
+    return x + y;
+}
 
-console.log(`Hello From ${ name }`);
+// TODO: innerhtml for add function
+
+// TODO: create an example string function for putting together myName and myAge
+
+// TODO: 
