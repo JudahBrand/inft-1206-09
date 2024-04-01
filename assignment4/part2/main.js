f300e1e35b1a21e7684c91fc52a0838f8486434b
JudahBrand <156016875+JudahBrand@users.noreply.github.com>
2024-04-01 15:50:27 -0400

    Finished image gallery on assignment 4

--- assignment4/part2/main.js
+++ assignment4/part2/main.js
@@ -4,34 +4,34 @@ const thumbBar = document.querySelector('.thumb-bar');
 const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
-/* Declaring the array of image filenames */
-const imageArray = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg']
+//image filenames
+const imageArray = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
 
-/* Declaring the alternative text for each image file */
-const altText = ['A beautiful landscape', ]
+// 
+const alttexts = ['A Close-up of a Human Eye', 'A Beautiful Landscape', 'Flowers in Nature', 'An Egyptian Hieroglyph', 'A Butterfly on a Leaf'];
 
 /* Looping through images */
 for (let i = 0; i < imageArray.length; i++) {
     const newImage = document.createElement('img');
     newImage.setAttribute('src', 'images/' + imageArray[i]);
-    newImage.setAttribute('alt', altText[i]);
+    newImage.setAttribute('alt', alttexts[i]);
     thumbBar.appendChild(newImage);
-    newImage.addEventListener('click', e => {
-        displayedImage.src = e.target.src
+    newImage.addEventListener('click', (e) => {
+        displayedImage.src = e.target.src;
     });
 }
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
-
 /* Wiring up the Darken/Lighten button */
-btn.addEventListener('click', () => {
-    const btnClass = btn.getAttribute('class');
-    if (btnClass === 'dark') {
+btn.addEventListener('click', (e) => {
+  if (e.target.className == 'dark') {
+    btn.setAttribute('class', 'light');
+    btn.textContent = 'Lighten';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+}
 
-    } else {
-
-    }
+ else {
+    btn.setAttribute('class', 'dark');
+    btn.textContent = 'Darken';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+}
 });
